f112bce3e7969ddbc2347c9ad3d7613372f8dc3d
zkly2005 <249843398@qq.com>
2020-02-25 11:50:51 +0800

    字符大小写转换

---
+++ char-change.py
@@ -0,0 +1,12 @@
+name=input("shuruP:")
+name_list=[]
+for i in name:
+    name_list.append(i)
+
+for i in range(len(name_list)):
+    if ord(name_list[i]) in range(65,91):
+        name_list[i]=chr(ord(name_list[i])+32)
+    elif ord(name_list[i]) in range(97,123):
+        name_list[i]=chr(ord(name_list[i])-32)
+sep=""
+print(sep.join(name_list))
