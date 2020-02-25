30ff93d8d33c6fd4cddc879286ff32ae3bfbe7e9
zkly2005 <61442088+zkly2005@users.noreply.github.com>
2020-02-25 15:02:47 +0800

    Update 字符串大小写转换.py

--- 字符串大小写转换.py
+++ 字符串大小写转换.py
@@ -1,8 +1,7 @@
-name=input("shuruP:")
+name=input("输入要转换的字符串:")
 name_list=[]
 for i in name:
     name_list.append(i)
-
 for i in range(len(name_list)):
     if ord(name_list[i]) in range(65,91):
         name_list[i]=chr(ord(name_list[i])+32)
